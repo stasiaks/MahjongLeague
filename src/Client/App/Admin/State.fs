5a5056bf3b3e215dad5bdb5f036aebfcee28ca62
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2020-03-26 19:04:22 +0100

    Fix Msg dispatching

--- src/Client/App/Admin/State.fs
+++ src/Client/App/Admin/State.fs
@@ -18,22 +18,22 @@ module Server =
 
 // defines the initial state and initial command (= side-effect) of the application
 let init(): State * Cmd<Msg> =
-    let initialModel = { Counter = None }
+    let state = { Counter = None }
     let loadCountCmd = Cmd.OfAsync.perform Server.api.initialCounter () InitialCountLoaded
-    initialModel, loadCountCmd
+    state, loadCountCmd
 
 // The update function computes the next state of the application based on the current state and the incoming events/messages
 // It can also run side-effects (encoded as commands) like calling the server via Http.
 // these commands in turn, can dispatch messages to which the update function will react.
-let update (msg: Msg) (currentModel: State): State * Cmd<Msg> =
-    match currentModel.Counter, msg with
+let update (msg: Msg) (state: State): State * Cmd<Msg> =
+    match state.Counter, msg with
     | Some counter, Increment ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value + 1 } }
-        nextModel, Cmd.none
+        let nextState = { state with Counter = Some { Value = counter.Value + 1 } }
+        nextState, Cmd.none
     | Some counter, Decrement ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value - 1 } }
-        nextModel, Cmd.none
+        let nextState = { state with Counter = Some { Value = counter.Value - 1 } }
+        nextState, Cmd.none
     | _, InitialCountLoaded initialCount ->
-        let nextModel = { Counter = Some initialCount }
-        nextModel, Cmd.none
-    | _ -> currentModel, Cmd.none
+        let nextState = { Counter = Some initialCount }
+        nextState, Cmd.none
+    | _ -> state, Cmd.none
