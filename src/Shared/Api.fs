3339aeb7e20e21e2c02813f6b2cb96ac16daafdc
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2020-03-29 18:46:21 +0200

    Split shared files

--- src/Shared/Api.fs
+++ src/Shared/Api.fs
@@ -1,7 +1,5 @@
 namespace Shared
 
-type Counter = { Value : int }
-
 module Route =
     /// Defines how routes are generated on server and mapped from client
     let builder typeName methodName =
@@ -11,4 +9,3 @@ module Route =
 /// to learn more, read the docs at https://zaid-ajaj.github.io/Fable.Remoting/src/basics.html
 type ICounterApi =
     { initialCounter : unit -> Async<Counter> }
-
---
+++ src/Shared/Types.fs
@@ -0,0 +1,12 @@
+namespace Shared
+
+type Counter =
+    { Value: int }
+
+/// Player's score at the end of the game
+[<Measure>]
+type Tenbo
+
+/// Points used to determine place in ranking
+[<Measure>]
+type Points
