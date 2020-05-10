68e5a30a33813d77ed8e18f45fbd537f7d3c3633
Sebastian Stasiak <arionw@live.com>
2020-05-11 00:04:54 +0200

    Translator pattern for admin page navigation

--- src/Client/App/Admin/Types.fs
+++ src/Client/App/Admin/Types.fs
@@ -7,10 +7,26 @@ type Page =
     | Dashboard
     | Users
 
-type Msg =
+type InternalMsg =
     | Increment
     | Decrement
     | InitialCountLoaded of Counter
+
+type OutMsg =
+    | NavigateTo of Page
+
+type Msg =
+    | ForSelf of InternalMsg
+    | ForParent of OutMsg
+
+type TranslationDictionary<'msg> =
+    { OnInternalMsg: InternalMsg -> 'msg
+      OnNavigateTo: Page -> 'msg }
+
+let translator dictionary msg =
+    match msg with
+        | ForSelf message -> dictionary.OnInternalMsg message
+        | ForParent (NavigateTo page) -> dictionary.OnNavigateTo page
 
 type State =
     { Counter: Counter option }
