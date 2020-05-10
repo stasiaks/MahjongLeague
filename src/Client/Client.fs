b517a437d3aee56d2408f6e731ac246bc688e0e0
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2020-05-10 23:35:32 +0200

    Show that you're logged in

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -20,5 +20,5 @@ Program.mkProgram init update render
 #endif
 |> Program.toNavigable parser urlUpdate
 |> Program.withReactBatched "elmish-app"
-|> Program.withSubscription App.State.onAuthenticated
+|> Program.withSubscription onAuthenticated
 |> Program.run
