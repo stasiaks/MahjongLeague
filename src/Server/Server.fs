ce4110eb2b291688911bbbcce433287789e0c5ee
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2020-09-18 09:46:25 +0200

    Update server project

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -3,23 +3,16 @@ open System.IO
 open Giraffe
 open Saturn
 
-let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
-
-let publicPath = Path.GetFullPath "../Client/public"
-
-let port =
-    "SERVER_PORT"
-    |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 8085us
 
 let combinedRouter =
     choose
         [ User.Router.router ]
 
 let app = application {
-    url ("http://0.0.0.0:" + port.ToString() + "/")
+    url ("http://0.0.0.0:8085")
     use_router combinedRouter
     memory_cache
-    use_static publicPath
+    use_static "public"
     use_gzip
 }
 
