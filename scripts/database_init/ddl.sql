4d28365b7164fc72f518cd8458b7aa7948e56564
Sebastian Stasiak <arionw@live.com>
2020-11-23 21:59:54 +0100

    Update script

--- scripts/database_init/ddl.sql
+++ scripts/database_init/ddl.sql
@@ -1,7 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE SCHEMA mahjong AUTHORIZATION test_app;
 
 CREATE TABLE mahjong.users (
-	id uuid NOT NULL,
+	id uuid NOT NULL DEFAULT uuid_generate_v1mc(),
 	objectid varchar(50) NULL,
 	"name" varchar(50) NOT NULL,
 	email varchar(250) NULL,
@@ -9,3 +11,18 @@ CREATE TABLE mahjong.users (
 	CONSTRAINT users_un UNIQUE (objectid)
 );
 
+CREATE TABLE mahjong.leagues (
+	id uuid NOT NULL DEFAULT uuid_generate_v1mc(),
+	"name" varchar(200) NOT NULL,
+	description varchar(500) NULL,
+	CONSTRAINT league_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE mahjong.seasons (
+	id uuid NOT NULL DEFAULT uuid_generate_v1mc(),
+	"name" varchar(50) NOT NULL,
+	date_from date NOT NULL,
+	date_to date NULL,
+	league_id uuid NOT NULL,
+	CONSTRAINT seasons_pk PRIMARY KEY (id)
+);
