8939414b415388c4639b84ab93f8f39a8b69c0d3
Sebastian Stasiak <arionw@live.com>
2020-11-23 21:57:28 +0100

    Seasons API

--- src/Shared/League.fs
+++ src/Shared/League.fs
@@ -4,9 +4,6 @@ open System
 
 type CreateLeagueModel = { Name: string; Description: string option }
 
-[<RequireQualifiedAccess>]
-type LeagueSeason = { Id: Guid; Name: string }
-
 type League =
     { Id: Guid
       Name: string
--- src/Shared/Season.fs
+++ src/Shared/Season.fs
@@ -7,12 +7,7 @@ type CreateSeasonModel =
     { Name: string
       LeagueId: string }
 
-[<RequireQualifiedAccess>]
-type SeasonLeague =
-    { Id: Guid
-      Name: string }
-
 type Season =
     { Id: Guid
       Name: string
-      League: SeasonLeague }
+      LeagueId: Guid }
